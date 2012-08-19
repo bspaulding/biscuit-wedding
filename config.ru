daf822b5e7e7b458b68c362a2f93f286240c6290
Bradley Spaulding <bspaulding@dataxu.com>
2012-08-19 10:31:06 -0500

    Added fresh rails app.

--- config.ru
+++ config.ru
@@ -1,2 +1,4 @@
-require 'rack'
-run Rack::Directory.new(".")
+# This file is used by Rack-based servers to start the application.
+
+require ::File.expand_path('../config/environment',  __FILE__)
+run BiscuitWedding::Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiscuitWedding::Application.config.secret_token = '4fa6f9808856650a71425227af908291f61c8bca6c271187a7f643e440dd5e529d8cec6e34ad00bed650f75a9c25b6ebf96eee03881ca461d4eac4949432bb2a'

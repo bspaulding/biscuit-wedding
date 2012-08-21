08bd8b38368fa47837d8c388289c24a6c87cd86a
Bradley Spaulding <bspaulding@dataxu.com>
2012-08-21 07:13:15 -0400

    Added rsvp stamp.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,6 @@ module ApplicationHelper
   def navigation_slugs
     slugs = BiscuitWedding::Application::STATIC_PAGES - ["coming_soon"]
 
-    slugs += ["rsvp"] if Property.check('allow-rsvp', 'true')
-
     slugs
   end
 end

629f45c0c81aa231c083da55c18b865f9a80429a
Bradley Spaulding <bspaulding@dataxu.com>
2012-08-27 19:49:39 -0400

    Added photos page.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ end
 
 module BiscuitWedding
   class Application < Rails::Application
-    STATIC_PAGES = ['coming_soon', 'our-story', 'registry', 'location', 'accommodations', 'contact_us']
+    STATIC_PAGES = ['coming_soon', 'our-story', 'registry', 'location', 'accommodations', 'contact_us', 'photos']
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

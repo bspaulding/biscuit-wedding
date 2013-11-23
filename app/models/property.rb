13fec1597f9c23146048c3a775fe0fbca5c0ebb5
Bradley J. Spaulding <bspaulding@dataxu.com>
2013-11-23 15:26:05 -0800

    Added property to toggle guestbook signing.

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,7 +2,8 @@ class Property < ActiveRecord::Base
   SUPPORTED_PROPERTIES = [
     { :name => "Allow RSVP", :key => 'allow-rsvp', :value => "false", :allowed_values => ["true", "false"] },
     { :name => "Site Launched", :key => 'site-launched', :value => "false", :allowed_values => ["true", "false"] },
-    { :name => "Menu Items Enabled", :key => 'menu-items-enabled', :value => "true", :allowed_values => ["true", "false"] }
+    { :name => "Menu Items Enabled", :key => 'menu-items-enabled', :value => "true", :allowed_values => ["true", "false"] },
+    { :name => "Allow Guestbook Signing", :key => 'allow-guestbook-signing', :value => "false", :allowed_values => ["true", "false"]  }
   ]
 
   validates_presence_of [:name, :value, :allowed_values]
